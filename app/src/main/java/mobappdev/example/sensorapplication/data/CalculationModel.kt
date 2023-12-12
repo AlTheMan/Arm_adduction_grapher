c2077a99086aeeba984bd66c6e8d8d924db48540
Emil Wennström <emilwennstrom@gmail.com>
2023-12-12 16:55:38 +0100

    <x

--- app/src/main/java/mobappdev/example/sensorapplication/data/CalculationModel.kt
+++ app/src/main/java/mobappdev/example/sensorapplication/data/CalculationModel.kt
@@ -1,8 +1,30 @@
 package mobappdev.example.sensorapplication.data
 
+import android.location.GnssMeasurement
+import android.util.Log
+import kotlin.math.atan
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+private const val TAG = "CalculationModel"
+
 object CalculationModel {
 
+    fun getLinAccAngle(measurement: Triple<Float, Float, Float>) : Float {
+        val norm = normVector(measurement)
 
+
+        return atan(measurement.first / distance(measurement.second, measurement.third))
+    }
+    private fun distance (y: Float, z: Float): Float {
+        val value =  sqrt(y.pow(2) + z.pow(2))
+        Log.d(TAG, value.toString())
+        return value
+    }
+
+    private fun normVector(m: Triple<Float, Float, Float>): Float {
+        return (m.first.pow(2) + m.second.pow(2) + m.third.pow(3))
+    }
 
 
 
